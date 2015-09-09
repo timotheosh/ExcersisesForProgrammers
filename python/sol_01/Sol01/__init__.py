f8bcbf97d1709ba41bdd3017217cdc7928474d8c
Tim Hawes <tim@selfdidactic.com>
2015-09-08 20:56:33 -0400

    Refinements

--- python/sol_01/Sol01/__init__.py
+++ python/sol_01/Sol01/__init__.py
@@ -4,17 +4,21 @@ from sys import exit
 
 class Sol01:
     def __init__(self, bill, tip_rate):
-        self.bill = bill
-        self.tip_rate = tip_rate
-        self.tip = None
-        self.total = None
-        self._calculate()
+        try:
+            self.bill = float(bill)
+            self.tip_rate = float(tip_rate)
+            self.tip = None
+            self.total = None
+            self._calculate()
+        except Exception,e:
+            print e.message
+            exit(1)
 
     def _calculate(self):
         try:
             tip = (float(self.tip_rate)/100.0) * self.bill
-            self.tip = float(format(tip, '.2f'))
-            self.total = self.bill + self.tip
+            self.tip = format(tip, '.2f')
+            self.total = format(self.bill + tip, '.2f')
         except Exception,e:
             print e.message
             exit(1)
@@ -23,9 +27,13 @@ class Sol01:
         print "Tip: %s" % self.tip
         print "Bill Total: %s" % self.total
 
-def main(bill, tip_rate):
+def main():
+    bill = raw_input("How much is the bill? ")
+    tip_rate = raw_input("What is the tip rate percentage (default 15)? ")
+    if not tip_rate:
+        tip_rate = 15
     calc = Sol01(bill,tip_rate)
     calc.display()
 
 if __name__ == "__main__":
-    main(10,15)
+    main()
