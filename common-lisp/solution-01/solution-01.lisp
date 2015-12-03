c8af27765983882bbba66ba5385674b5f8af4550
Tim Hawes <tim@selfdidactic.com>
2015-12-02 22:39:43 -0500

    Added common lisp solutions.

---
+++ common-lisp/solution-01/solution-01.lisp
@@ -0,0 +1,27 @@
+;; Solution 1
+
+(defun enter-bill-amount ()
+  (princ "Enter the bill amount: ")
+  (let ((bill-amount (read-line)))
+    (unless (zerop (length bill-amount))
+      (read-from-string bill-amount))))
+
+(defun enter-tip-rate ()
+  (princ "Enter the tip rate (without the %, default is 15): ")
+  (let ((tip-rate (read-line)))
+    (if (zerop (length tip-rate))
+        0.15
+        (/ (read-from-string tip-rate) 100.00))))
+
+(defun get-total (bill tip-rate)
+  "bill and tip rate should be floats. tip rate should be a
+   percentage, already divided by 100"
+  (* bill (+ 1 tip-rate)))
+
+(defun tip-calculator ()
+  (princ "What is the bill amount?")
+  (read-line bill)
+  (fresh-line)
+  (princ "What is the tip rate?")
+  (read-line tip-rate)
+  (format t "The total is ~A" (get-total bill tip-rate)))
