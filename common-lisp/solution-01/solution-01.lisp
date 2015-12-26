fd8e5e045f2d9fb9d654d37a1b6ab676c5bfde17
Tim Hawes <tim@selfdidactic.com>
2015-12-25 23:00:30 -0500

    Now prevents entry of negative numbers.

--- common-lisp/solution-01/solution-01.lisp
+++ common-lisp/solution-01/solution-01.lisp
@@ -14,40 +14,52 @@
               (values (float (+ integer (/ fraction (expt 10 (- j i 1))))) j))))))
 
 (defun prompt-read (prompt)
-  (format *query-io* "~a: " prompt)
+  (format *query-io* "~&~a: " prompt)
   (force-output *query-io*)
   (read-line *query-io*))
 
 (defun enter-bill-amount ()
   "Enter the bill amount."
-  (let ((bill-amount 0))
+  (let ((bill-amount nil))
     (loop
-       (if (zerop bill-amount)
+       (if (or (null bill-amount) (minusp bill-amount))
            (progn
-             (setf bill-amount (or
-                                (parse-float (prompt-read "Enter bill amount"))
-                                0))
-             (when (zerop bill-amount)
-               (format t "~%That is not a valid amount! Digits only, please.~%")))
+             (setf bill-amount (parse-float (prompt-read "Enter bill amount")))
+             (when (or (not bill-amount) (and bill-amount (< bill-amount 0)))
+               (format t "That is not a valid amount! Digits only (no negative numbers), please.~%")))
            (return)))
     (setf bill-amount (read-from-string (format nil "~$" bill-amount)))))
+
+(defun enter-tip-rate-old ()
+  "Enter tip rate."
+  (let ((tip-rate nil))
+    (loop
+       (if (or (null tip-rate) (minusp tip-rate))
+           (let ((line
+                  (prompt-read "Enter the tip rate (without the %, default is 15)")))
+             (when (zerop (length (string-trim #(#\space #\tab) line)))
+               (setf tip-rate 15)
+               (return))
+             (setf line (parse-float line))
+             (if (not line)
+                 (format t "That is not a valid amount! Digits only, please.~%")
+                 (progn
+                   (setf tip-rate line)
+                   (return))))))
+    (/ tip-rate 100.0)))
 
 (defun enter-tip-rate ()
   "Enter tip rate."
   (let ((tip-rate nil))
     (loop
-       (if (not tip-rate)
-           (let ((d (prompt-read "Enter the tip rate (without the %, default is 15)")))
-             (when (zerop (length d))
-               (progn
-                 (setf tip-rate 15)
-                 (return)))
-             (setf d (or (parse-float d) nil))
-             (if (not d)
-                 (format t "~%That is not a valid amount! Digits only, please.~%")
-                 (progn
-                   (setf tip-rate d)
-                   (return))))))
+      (let ((line (prompt-read "Enter the tip rate (without the %, default is 15)")))
+        (when (zerop (length (string-trim #(#\space #\tab) line)))
+          (setf tip-rate 15)
+          (return))
+        (setf tip-rate (parse-float line))
+        (if (and tip-rate (minusp tip-rate))
+            (format t "That is not a valid amount! Digits only, please.~%")
+            (return))))
     (/ tip-rate 100.0)))
 
 (defun tip-amount (bill tip-rate)
@@ -61,3 +73,5 @@ tip-rate is a percentage, already divided by 100."
          (tip (tip-amount bill tip-rate)))
     (format t "The tip is ~,2F~%" tip)
     (format t "The total is ~,2F" (+ bill (* bill tip-rate)))))
+
+(tip-calculator)
