1e668d7d6480abf9b017cb4a84bc04ca6667a679
Tim Hawes <tim@selfdidactic.com>
2017-06-19 19:25:58 -0400

    progress

--- 30DaysOfCode/Day08/dictionaries-and-maps.rkt
+++ 30DaysOfCode/Day08/dictionaries-and-maps.rkt
@@ -1,22 +1,16 @@
 #lang racket
 
-;; Function with recursion for prompting until only enter is returned.
-(define (add-key searches)
-  (let ((key (read-line)))
-    (if (and (not (eof-object? key))
-             (> (string-length key) 0))
-        (begin
-          (set! searches (cons key searches))
-          (add-key searches))
-            searches)))
-
 (let ((number (string->number (read-line)))
-      (contacts '()))
+      (contacts (make-hash)))
   (for ([num number])
-    (set! contacts (cons (string-split (read-line)) contacts)))
-  (let ((searches (reverse (add-key '()))))
-    (for ([key searches])
-      (let ((result (assoc key contacts)))
-        (if (not result)
-            (display (format "Not found~%"))
-            (display (format "~A=~A~%" (first result) (second result))))))))
+    (let ((entry (string-split (read-line))))
+      (hash-set! contacts (first entry) (second entry))))
+  (let loop ([prompt #f])
+    (cond [(false? prompt) (loop (read-line))]
+          [(and (not (eof-object? prompt))
+                (> (string-length prompt) 0))
+           (begin
+             (if (not (hash-has-key? contacts prompt))
+                 (display (format "Not found~%"))
+                 (display (format "~A=~A~%" prompt (hash-ref contacts prompt))))
+             (loop (read-line)))])))
