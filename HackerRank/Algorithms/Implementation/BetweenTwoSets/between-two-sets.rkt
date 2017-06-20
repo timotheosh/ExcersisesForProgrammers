3d1d14065088a28634e0c29039e3df5f661a4267
Tim Hawes <tim@selfdidactic.com>
2017-06-20 13:43:06 -0400

    progress

---
+++ HackerRank/Algorithms/Implementation/BetweenTwoSets/between-two-sets.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+(define garbage (read-line))
+(let ((a (cons (map string->number (string-split (read-line)))))
+      (b (cons (map string->number (string-split (read-line))))))
+  (let* ((x1 0)
+         (x2 0)
+         (hi (first (sort x2))))
+    ))
