2ca1854e42393dba770c41f2ef03d565f66c6081
Tim Hawes <tim@selfdidactic.com>
2017-07-05 21:39:50 -0400

    progress

--- 30DaysOfCode/Day25/prime-numbers.rkt
+++ 30DaysOfCode/Day25/prime-numbers.rkt
@@ -1,18 +1,26 @@
 #lang racket
 
-(define (is-prime n)
-  (let loop ((x n)
-             (y 2))
-    (cond [(or (< x 2)
-               (= x y))
-           #t]
-          [(= (modulo x y) 0)
-           #f]
-          [else
-           (loop x (+ y 1))])))
+(define (square x)
+  (* x x))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (find-divisor n test-divisor)
+  (cond [(> (square test-divisor) n) n]
+        [(divides? test-divisor n) test-divisor]
+        [else (find-divisor n (+ test-divisor 1))]))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (prime? n)
+  (if (= (abs n) 1)
+      #f
+      (= n (smallest-divisor n))))
 
 (for ([num (string->number (read-line))])
   (let ((message "Not prime"))
-    (when (is-prime (string->number (read-line)))
+    (when (prime? (string->number (read-line)))
       (set! message "Prime"))
     (displayln (format "~A" message))))
