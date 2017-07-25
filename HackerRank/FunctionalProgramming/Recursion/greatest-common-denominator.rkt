24ae1ee4a59237b17c5d58e8477070dcbc10442a
Tim Hawes <tim.hawes@inin.com>
2017-07-25 09:00:19 -0400

    Correct error

--- HackerRank/FunctionalProgramming/Recursion/greatest-common-denominator.rkt
+++ HackerRank/FunctionalProgramming/Recursion/greatest-common-denominator.rkt
@@ -4,7 +4,7 @@
   (displayln (format "~A ~A" g l))
   (if (= (modulo g l) 0)
       l
-      (gcd l (modulo g l))))
+      (my-gcd l (modulo g l))))
 
 (let ((nums (map string->number (string-split (read-line)))))
   (gcd (apply max nums) (apply min nums)))
